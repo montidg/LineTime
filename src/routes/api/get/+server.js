8fdf8f64a0b847299cf5db8fd1cc1a3f4e44d0bd
montidg <dravenmonti@gmail.com>
2023-05-23 20:12:36 -0400

    Fix build issue

--- src/routes/api/get/+server.js
+++ src/routes/api/get/+server.js
@@ -1,8 +1,9 @@
 import { getDb } from '$lib/db.js';
-let db = await getDb();
+let db
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ request }) {
+    db = await getDb();
     let data = await db.all('SELECT * FROM events');
 
     return new Response(JSON.stringify(data));
