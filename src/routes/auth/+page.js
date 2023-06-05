3a10b17ee43ad73904ee9173f11491d4ebdb492b
montidg <dravenmonti@gmail.com>
2023-06-04 20:44:15 -0400

    why wont this stupid thing work

--- src/routes/auth/+page.js
+++ src/routes/auth/+page.js
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 /** @type {import('./$types').PageLoad} */
 export async function load({ cookies,url }) {
     let param = url.searchParams.get('data');
-    cookies.set('token',param, { path: '/', maxAge: 60 * 60 * 24 * 7, secure: false });
+    cookies.set('token',param, { path: '/', maxAge: 60 * 60 * 24 * 7, secure: false, httpOnly: false });
     throw redirect(302, '/');
 }
